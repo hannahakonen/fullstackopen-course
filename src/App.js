f6e4a2fd930aa448cff523688c35e8ba3b75cc27
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2023-12-27 16:10:22 +0200

    osa1 1.8 unicafe step3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -22,14 +43,8 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good' />
       <Button handleClick={handleNeutralClick} text='neutral' />  
       <Button handleClick={handleBadClick} text='bad' />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
-    </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
   )
 }
 
