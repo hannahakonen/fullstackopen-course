67f383d73d789c9b9a2066a0406bdaaa9bb3b298
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2023-12-27 16:21:18 +0200

    osa1 1.10 unicafe step5

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticsLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
     return (
@@ -17,12 +25,12 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.good + props.neutral + props.bad}</p>
-      <p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
-      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100} %</p>
+      <StatisticsLine text='good' value={props.good} />
+      <StatisticsLine text='neutral' value={props.neutral} /> 
+      <StatisticsLine text='bad' value={props.bad} />
+      <StatisticsLine text='all' value={props.good + props.neutral + props.bad} />
+      <StatisticsLine text='average' value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+      <StatisticsLine text='positive' value={props.good / (props.good + props.neutral + props.bad) * 100 + ' %'} />
     </div>
   )
 }
