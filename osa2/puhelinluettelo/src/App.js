b413ae686476a5f3671c2f2fbd477e01552db92b
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2024-01-02 18:11:50 +0200

    osa2 2.10 puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,16 +1,43 @@
 import { useState } from 'react'
 
+const PersonForm = ({ addPerson, newName, handlePersonChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handlePersonChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
 const Person = ({ person }) => {
   return (
     <p>{person.name} {person.number}</p>
   )
-} 
+}
+
+const Persons = ({ persons }) => { 
+  return (
+    <div>
+      {persons.map(person => <Person key={person.name} person={person} />
+      )}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '040-123456' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -38,27 +65,28 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+
       <div>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
-        )}
+        filter shown with - not ready<input />
       </div>
+
+      <h2>Add a new</h2>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons persons={persons} />
     </div>
   )
 
