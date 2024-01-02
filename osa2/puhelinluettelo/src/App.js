571042c6ccf127d90b5c9a090620c4b4ed257e0c
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2024-01-02 13:10:25 +0200

    osa2 2.6 puhelinluettelo step1 v2

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+} 
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id: 1 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
